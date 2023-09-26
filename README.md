99a061714e34c4d50ded63dc570954503343d7d3
Hansimov <591172499@qq.com>
2023-09-26 17:53:25 +0800

    [Refactor] Modurarize screen shot and captcha locate and click

--- README.md
+++ README.md
@@ -3,5 +3,11 @@ Auto click CAPTCHA of "Verify you are human" by CLOUDFLARE
 
 ## Installations
 ```
-pip install pyautogui mss screeninfo Pillow opencv-python
+pip install pyautogui mss Pillow opencv-python
+```
+
+## Run
+
+```
+python bypass_captcha.py
 ```
--- bypass_captcha.py
+++ bypass_captcha.py
@@ -1,14 +1,15 @@
-import pyautogui
 import cv2
+import pyautogui
+from mss import mss
 from pathlib import Path
 from PIL import Image, ImageGrab
-from mss import mss
 
 
 class ImageMatcher:
     def __init__(self, source_image_path, template_image_path):
         self.source_image = cv2.imread(str(source_image_path))
         self.template_image = cv2.imread(str(template_image_path))
+        self.detected_image_path = source_image_path.parent / "screenshot_detected.png"
 
     def match(self):
         """
@@ -38,7 +39,7 @@ class ImageMatcher:
             color=(0, 255, 0),
             thickness=2,
         )  # BGR
-        # return rectangle_region
+        cv2.imwrite(str(self.detected_image_path), self.source_image)
 
     def display(self):
         cv2.imshow("Detected", self.source_image)
@@ -59,9 +60,8 @@ class CaptchaBypasser:
     def get_captcha_location(self):
         with mss() as sct:
             all_monitor = sct.monitors[0]
-            # print(all_monitor["top"], all_monitor["left"])
-            monitor_top = all_monitor["top"]
-            monitor_left = all_monitor["left"]
+            monitor_left_offset = all_monitor["left"]
+            monitor_top_offset = all_monitor["top"]
 
         image_matcher = ImageMatcher(
             source_image_path=self.screen_shot_image_path,
@@ -70,11 +70,30 @@ class CaptchaBypasser:
 
         match_region = image_matcher.match()
         image_matcher.draw_rectangle()
-        image_matcher.display()
-        # print(self.captcha_location)
+        # image_matcher.display()
+        match_region_in_monitor = (
+            match_region[0] + monitor_left_offset,
+            match_region[1] + monitor_top_offset,
+            match_region[2] + monitor_left_offset,
+            match_region[3] + monitor_top_offset,
+        )
+        checkbox_center = (
+            (match_region_in_monitor[0] + match_region_in_monitor[2]) / 2,
+            (match_region_in_monitor[1] + match_region_in_monitor[3]) / 2,
+        )
+        return checkbox_center
+
+    def click_target_checkbox(self):
+        captcha_checkbox_center = self.get_captcha_location()
+        pyautogui.moveTo(*captcha_checkbox_center)
+        pyautogui.click()
+
+    def run(self):
+        self.get_screen_shots()
+        self.get_captcha_location()
+        self.click_target_checkbox()
 
 
 if __name__ == "__main__":
     captcha_bypasser = CaptchaBypasser()
-    captcha_bypasser.get_screen_shots()
-    captcha_bypasser.get_captcha_location()
+    captcha_bypasser.run()
